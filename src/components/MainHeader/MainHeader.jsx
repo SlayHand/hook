1c889eb9829bce01383149c0eb5792977caf4a31
fredy <fredy-andri.mottus@voco.ee>
2025-03-20 10:03:15 +0200

    esimene ülesanne main header.jsx sisu

---
+++ src/components/MainHeader/MainHeader.jsx
@@ -0,0 +1,11 @@
+import './MainHeader.css'
+
+const MainHeader = () => {
+    return (
+        <header className='main-header'>
+            <h1>A Typical Page</h1>
+        </header>
+    )
+}
+
+export default MainHeader
